1c49d36f149ff7f8a478c413a85637b38ed9832e
“noarrassam” <noor.s.rassam@gmail.com>
2022-04-27 13:58:05 -0400

    isPrimeEx

--- index2.js
+++ index2.js
@@ -1,9 +1,10 @@
-function hashStringToInt(key, val) {
+function hashStringToInt(key, length) {
   let hash = 0;
   for (let i = 0; i < key.length; i++) {
     hash += key.charCodeAt(i);
   }
-  return hash % val;
+
+  return hash % length;
 }
 
 class HashTable {
@@ -13,15 +14,16 @@ class HashTable {
 
   set(key, value) {
     let idx = hashStringToInt(key, this.data.length);
+
     if (this.data[idx]) {
-      this.data[idx].push([key, value]);
+      this.data[idx].push(key, value);
     } else {
       this.data[idx] = [[key, value]];
     }
   }
 
   get(key) {
-    const idx = hashStringToInt(key, this.data.length);
+    let idx = hashStringToInt(key, this.data.length);
 
     if (!this.data[idx]) {
       return null;
@@ -31,8 +33,10 @@ class HashTable {
   }
 }
 
-let map = new HashTable(20);
-
-map.set("apple", 3);
-console.log(map);
-console.log(map.get("apple"));
+let table = new HashTable(20);
+table.set("apple", 3);
+console.log(table);
+console.log(table.get("apple"));
+table.set("tomato", 4);
+console.log(table);
+console.log(table.get("tomato"));
